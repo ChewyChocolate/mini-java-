cf45a4a5c2d996ea93d8e0f298ca9a1b7a3d8985
ChewyChocolate <116084618+ChewyChocolate@users.noreply.github.com>
2023-09-30 13:59:35 +0800

    Update Computer_Guess_the_Number.java

--- Computer_Guess_the_Number.java
+++ Computer_Guess_the_Number.java
@@ -7,16 +7,19 @@ public class Main {
         int left = 0;
         int right = 100;
         int guess = (left + right) / 2; 
+        int guessCount = 1;
         int ans;
 
         while (true) {
             System.out.print("\033[H\033[2J"); 
             System.out.println("My guess is " + guess);
+            System.out.println("Guess count: " + guessCount);
             System.out.println("1. If the answer is lower");
             System.out.println("2. If the answer is higher");
             System.out.println("3. If the answer is correct");
 
             ans = scan.nextInt();
+            guessCount++;
 
             if (ans == 1) {
                 right = guess - 1; 
@@ -25,7 +28,9 @@ public class Main {
                 left = guess + 1;
                 guess = (left + right) / 2;
             } else if (ans == 3) {
+                System.out.print("\033[H\033[2J");
                 System.out.println("The answer is correct!");
+                System.out.print("My total number of guess: " + guessCount);
                 break;
             } else {
                 System.out.println("Invalid input. Please enter 1, 2, or 3.");
